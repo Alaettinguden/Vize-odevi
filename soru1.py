1402299a893c267c91ae0b9323a8d66e05ac6fd5
Alaettinguden <74980213+Alaettinguden@users.noreply.github.com>
2020-12-20 02:43:06 +0300

    Add files via upload

---
+++ soru1.py
@@ -0,0 +1,12 @@
+﻿# 1. Soru 
+
+import re
+
+exp = '^[a-z0-9\._]+[@]\w+[.]\w{2,3}([.]\w{2})?$'
+def validateEMail(eMail):
+    if (re.search(exp, eMail)):
+    	print("E-Posta adresi doğrulandı")
+    else: 
+    	print("E-Posta adresi hatalı!")
+
+validateEMail("ali.erbey@usak.edu.tr")
---
+++ soru3.py
@@ -0,0 +1,10 @@
+﻿# 3. Soru 
+txt = "USAK Universitesi"
+search = "ver"
+pos = txt.find(search)
+if (pos > 0):
+	pre = txt[pos - 1]
+	post = txt[pos + len(search)]
+	print(pre + search + post)
+else:
+	print("Aranan değer bulunamadı!")
